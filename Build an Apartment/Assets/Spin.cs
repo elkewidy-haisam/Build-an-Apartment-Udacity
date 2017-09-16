0f675997728625672242808169b31178e7174f78
Haisam <elkewidyhaisam@gmail.com>
2017-09-16 11:05:15 -0400

    commit

---
+++ Build an Apartment/Assets/Spin.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Spin : MonoBehaviour
+{
+
+    public bool rotateObject = true;
+    private Button mouseButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        mouseButton = GetComponent<Button>();
+        mouseButton.onClick.AddListener(invertBoolean);
+    }
+
+    void invertBoolean()
+    {
+        rotateObject = !rotateObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+
+        if (rotateObject == true)
+        {
+            transform.Rotate(new Vector3(Time.deltaTime * 0, 4, 0));
+        }
+
+
+    }
+}
